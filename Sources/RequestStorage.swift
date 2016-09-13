c739fe455ad79e6b5b5dd85818fe5860486255db
Vadym Markov <markov.vadym@gmail.com>
2016-09-13 11:37:34 +0200

    Request storage

--- Sources/RequestStorage.swift
+++ Sources/RequestStorage.swift
@@ -1,9 +1,63 @@
-//
-//  RequestStorage.swift
-//  Offline
-//
-//  Created by Vadym Markov on 13/09/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
+
+public final class RequestStorage {
+  let key = "Offline.RequestStorage"
+
+  public static let shared: RequestStorage = RequestStorage()
+
+  public private(set) var requests = [String: NSURLRequest]()
+
+  private var userDefaults: NSUserDefaults {
+    return NSUserDefaults.standardUserDefaults()
+  }
+
+  // MARK: - Initialization
+
+  init() {
+    requests = load()
+  }
+
+  // MARK: - Save
+
+  public func save(request: NSURLRequest) {
+    guard let key = request.URL?.absoluteString else {
+      return
+    }
+
+    requests[key] = request
+    saveAll()
+  }
+
+  public func saveAll() {
+    let data = NSKeyedArchiver.archivedDataWithRootObject(requests)
+    userDefaults.setObject(data, forKey: key)
+    userDefaults.synchronize()
+  }
+
+  // MARK: - Remove
+
+  public func remove(request: NSURLRequest) {
+    guard let key = request.URL?.absoluteString else {
+      return
+    }
+
+    requests.removeValueForKey(key)
+    saveAll()
+  }
+
+  public func clear() {
+    requests.removeAll()
+    userDefaults.removeObjectForKey(key)
+    userDefaults.synchronize()
+  }
+
+  // MARK: - Load
+
+  func load() -> [String: NSURLRequest] {
+    guard let data = userDefaults.objectForKey(key) as? NSData,
+      dictionary = NSKeyedUnarchiver.unarchiveObjectWithData(data) as? [String: NSURLRequest]
+      else { return [:] }
+
+    return dictionary
+  }
+}
