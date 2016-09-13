a5d6a30af8af3e51c99003273878216c9ead5c61
Vadym Markov <markov.vadym@gmail.com>
2016-09-13 11:37:21 +0200

    Data operation

--- Sources/Operation/DataOperation.swift
+++ Sources/Operation/DataOperation.swift
@@ -1,9 +1,31 @@
-//
-//  NetworkDataOperation.swift
-//  Offline
-//
-//  Created by Vadym Markov on 13/09/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
+
+public typealias DataTaskCompletion = (NSData?, NSURLResponse?, NSError?) -> Void
+
+class DataOperation: ConcurrentOperation {
+
+  private let session: NSURLSession
+  private let request: NSURLRequest
+  private let completion: DataTaskCompletion
+  private var task: NSURLSessionDataTask?
+
+  init(session: NSURLSession, request: NSURLRequest, completion: DataTaskCompletion) {
+    self.session = session
+    self.request = request
+    self.completion = completion
+  }
+
+  override func execute() {
+    task = session.dataTaskWithRequest(request) { [weak self] (data, response, error) in
+      self?.completion(data, response, error)
+      self?.state = .Finished
+    }
+
+    task?.resume()
+  }
+
+  override func cancel() {
+    super.cancel()
+    task?.cancel()
+  }
+}
