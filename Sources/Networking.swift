b024738b265fa2c1eebc76e213d7ace0398a9ff4
Vadym Markov <markov.vadym@gmail.com>
2016-09-13 11:36:43 +0200

    Implement networking

--- Sources/Networking.swift
+++ Sources/Networking.swift
@@ -1,9 +1,45 @@
-//
-//  Networking.swift
-//  Offline
-//
-//  Created by Vadym Markov on 13/09/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
+
+public final class Networking {
+
+  public enum Kind {
+    case Sync, Async, Limited(Int)
+  }
+
+  public let kind: Kind
+  private let session: NSURLSession
+  private let queue: NSOperationQueue
+
+  // MARK: - Initialization
+
+  public init(kind: Kind, session: NSURLSession = NSURLSession.sharedSession()) {
+    self.kind = kind
+    self.session = session
+    queue = NSOperationQueue()
+
+    switch kind {
+    case .Sync:
+      queue.maxConcurrentOperationCount = 1
+    case .Async:
+      queue.maxConcurrentOperationCount = -1
+    case .Limited(let count):
+      queue.maxConcurrentOperationCount = count
+    }
+  }
+
+  // MARK: - Data request
+
+  public func send(request: NSURLRequest, saveOffline: Bool = false, completion: DataTaskCompletion) {
+    let operation = DataOperation(session: session, request: request) {
+      data, response, error in
+
+      if saveOffline && error?.isOffline == true {
+        RequestStorage.shared.save(request)
+      }
+
+      completion(data, response, error)
+    }
+
+    queue.addOperation(operation)
+  }
+}
